5ff1041d0814b5755cdf941fb0d7bb6ac0c351a9
startisaac <isaac.orlando10@gmail.com>
2020-04-09 12:16:45 -0700

    outputing either even or odd

---
+++ myfirsttask.js
@@ -0,0 +1,17 @@
+
+
+// this are my input
+let myName = "isaac orlando";
+let myCourse = ['html', 'css', 'js',];
+
+//thisare my output
+console.log(`my name is ${myName}`);
+console.log(`These are my courses ${myCourse}`);
+
+//i used a for loop
+for ( let i=0; i<=200; i++){
+    if(myCourse.length % 2 === i % 2){
+        console.log(i)
+    }
+
+}
